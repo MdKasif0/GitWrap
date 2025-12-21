e2367aae2a7eb102f20ed1ddb92936b776fb0564
Md Kasif Uddin <mdkasifuddin123@gmail.com>
2025-12-21 07:07:37 +0000

    Use different model

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,5 +3,5 @@ import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
   plugins: [googleAI({apiVersion: "v1beta"})],
-  model: 'googleai/gemini-2.5-flash',
+  model: 'googleai/gemini-pro',
 });
