d8c00a4ac91872e088c15562b76ecc634d3049a9
Md Kasif Uddin <mdkasifuddin123@gmail.com>
2025-12-21 20:32:31 +0000

    Add Buy me a Coffee after cup icon and make whole of them as a button

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,9 +8,10 @@ export function Header() {
     <header className="absolute top-0 left-0 right-0 z-20 p-4 flex justify-between items-center">
       <Logo />
       <div className="flex items-center gap-2">
-        <Button variant="ghost" size="icon" asChild>
+        <Button variant="ghost" asChild>
           <Link href="/support" aria-label="Support page">
             <Coffee />
+            Buy me a Coffee
           </Link>
         </Button>
       </div>
