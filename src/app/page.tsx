eb33e477b46541697dadfe02d7bfaf117caae78f
Md Kasif Uddin <mdkasifuddin123@gmail.com>
2025-12-21 04:20:35 +0000

    Remove that gift icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,6 @@ import { SeriousnessIcon } from "@/components/icons/seriousness-icon";
 import { ShareIcon } from "@/components/icons/share-icon";
 import Image from "next/image";
 import { Header } from "@/components/header";
-import { GiftIcon } from "@/components/icons/gift-icon";
 
 export default function Home() {
   const features = [
@@ -42,7 +41,7 @@ export default function Home() {
         <div className="flex flex-col items-center space-y-4 text-center">
           <div className="relative">
             <h1 className="text-6xl font-bold tracking-tight bg-gradient-to-br from-green-300 via-primary to-green-600 bg-clip-text text-transparent sm:text-8xl flex items-center gap-4">
-              GitHub Wrapped <GiftIcon className="text-4xl sm:text-6xl text-primary" />
+              GitHub Wrapped
             </h1>
              <div className="absolute -top-4 -left-8 sm:-top-5 sm:-left-12 text-sm sm:text-base font-bold bg-white/20 text-white px-3 py-1 rounded-full backdrop-blur-sm -rotate-12">
               2025
